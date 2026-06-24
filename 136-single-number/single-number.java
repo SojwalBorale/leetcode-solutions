0c690e6bd09654350af56bbbd108df57f689563c
SojwalBorale <107742891+SojwalBorale@users.noreply.github.com>
2026-06-24 12:48:01 +0530

    Time: 1 ms (99.97%) | Memory: 46.4 MB (98.55%) - LeetSync

---
+++ 136-single-number/single-number.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+           int op = nums[0];
+    for(int i=1;i<nums.length;i++){
+            op ^= nums[i];
+    }
+    return op;
+    }
+}
