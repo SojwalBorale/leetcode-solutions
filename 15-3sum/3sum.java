f889c7af505cafc4bc84c1272a362da98dbd22ff
SojwalBorale <107742891+SojwalBorale@users.noreply.github.com>
2026-06-29 10:41:37 +0530

    Time: 30 ms (88.41%) | Memory: 59.4 MB (24.88%) - LeetSync

---
+++ 15-3sum/3sum.java
@@ -0,0 +1,80 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+       List<List<Integer>> result = new ArrayList<>();
+       Arrays.sort(nums);
+
+       int n= nums.length;
+
+       for(int i=0;i<n;i++){
+        // skip duplicate nums[i]
+        if(i>0 && nums[i] == nums[i-1]) continue;
+        
+        int left = i+1;
+        int right = n-1;
+
+        while(left<right){
+            int sum = nums[i]+nums[left]+nums[right];
+            if(sum < 0){
+                left++;
+            }else if(sum >0){
+                right--;
+            }else{
+                result.add(Arrays.asList(nums[i],nums[left],nums[right]));
+                left++;
+                right--;
+
+                while(left<right && nums[left]==nums[left-1]) left++;
+            }
+        }
+       }
+       return result;
+    }
+}
+
+//  Set<List<Integer>> result = new HashSet<>();
+//         int n = nums.length;
+//         for(int i=0;i<n;i++){
+//             for(int j=i+1;j<n;j++){
+//                 for(int k=j+1;k<n;k++){
+//                     if(nums[i]+nums[j]+nums[k]==0){
+//                         List<Integer> triplet = Arrays.asList(nums[i],nums[j],nums[k]);
+//                         Collections.sort(triplet);
+//                         result.add(triplet);
+//                     }
+//                 }
+//             }
+//         }
+//         return new ArrayList<>(result);         time complexity - O(n^3)
+
+
+//   List<List<Integer>> result = new ArrayList<>();           same apppraoch 2 pointer but loop changed
+//         Arrays.sort(nums); // Step 1: sort array
+//         int n = nums.length;
+
+//         for (int i = 0; i < n - 2; i++) {
+//             // Skip duplicate nums[i]
+//             if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+//             int left = i + 1;
+//             int right = n - 1;
+
+//             while (left < right) {
+//                 int sum = nums[i] + nums[left] + nums[right];
+
+//                 if (sum == 0) {
+//                     result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+
+//                     // Skip duplicates for left and right
+//                     while (left < right && nums[left] == nums[left + 1]) left++;
+//                     while (left < right && nums[right] == nums[right - 1]) right--;
+
+//                     left++;
+//                     right--;
+//                 } else if (sum < 0) {
+//                     left++;  // need bigger sum
+//                 } else {
+//                     right--; // need smaller sum
+//                 }
+//             }
+//         }
+//         return result;
